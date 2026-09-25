d53734fe8ad3ac94a522411176c9e6148aa92a50
Farhanul K. Islam <167823096+Farhanul199@users.noreply.github.com>
2026-09-25 18:50:20 +0100

    Add files via upload

--- assets/js/wwp-core.js
+++ assets/js/wwp-core.js
@@ -261,8 +261,8 @@ const FEATURE_MODULES = {
   journal: { js:['/assets/js/features/journal.js?v=4'],      css:['/assets/css/features/journal.css?v=2'] },
   dua:     { js:['/assets/js/features/dua.js?v=4'],          css:['/assets/css/features/dua.css?v=1'] },
   guides:  { js:['/assets/js/features/guides.js?v=4'],       css:['/assets/css/features/guides.css?v=2'] },
-  mosque:  { js:['/assets/js/features/find-a-mosque.js?v=13'],css:['/assets/css/features/find-a-mosque.css?v=5'] },
-  travel:  { js:['/assets/js/features/travel-mode.js?v=5'],  css:['/assets/css/features/travel-mode.css?v=5'] },
+  mosque:  { js:['/assets/js/features/find-a-mosque.js?v=14'],css:['/assets/css/features/find-a-mosque.css?v=5'] },
+  travel:  { js:['/assets/js/features/travel-mode.js?v=6'],  css:['/assets/css/features/travel-mode.css?v=5'] },
   community:{js:['/assets/js/features/community.js?v=2'],    css:['/assets/css/features/community.css?v=1'] }
 };
 const loadedModules = new Set();
